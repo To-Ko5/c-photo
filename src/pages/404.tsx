f02a9d61b02c272506ed1b06e8f3906bd6bf6658
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-01-11 23:18:13 +0900

    404ページ追加

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layouts from '../components/layouts'
+
+export default () => {
+  return (
+    <Layouts>
+      <div className="404">
+        <h1 className="about__title">お探しのページが見つかりませんでした。</h1>
+      </div>
+    </Layouts>
+  )
+}
