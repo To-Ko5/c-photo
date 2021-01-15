42af656e575658616d215443dcdae67fe9ee823a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-01-15 21:09:56 +0900

    footer components追加

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Footer from '../styles/footer.module.scss'
+
+export default () => {
+  return (
+    <footer className={Footer.footer}>
+      <p className={Footer.footer__text}>
+        <small>C-photo</small>
+      </p>
+    </footer>
+  )
+}
