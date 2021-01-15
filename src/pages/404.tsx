cc71adff5f19c8908b116d1bdb7c26114be17dc7
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-01-15 21:09:35 +0900

    head components追加

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
 import Layouts from '../components/layouts'
+import Head from '../components/head'
 
-export default () => {
+export default ({ location }) => {
   return (
     <Layouts>
+      <Head
+        pageTitle="ページが見つかりません"
+        pageDescription={null}
+        pagePath={location.pathname}
+        pageImg={null}
+        pageImgHeight={null}
+        pageImgWidth={null}
+      />
       <div className="404">
         <h1 className="about__title">お探しのページが見つかりませんでした。</h1>
       </div>
