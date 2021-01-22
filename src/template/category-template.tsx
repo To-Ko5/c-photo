cd159ddbea533250a9874f9d1ba97fa667e7c62b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-01-22 16:36:39 +0900

    category-template 修正

--- src/template/category-template.tsx
+++ src/template/category-template.tsx
@@ -1,23 +1,60 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
+// components
 import Layouts from '../components/layouts'
+import Keyvisual from '../components/category/keyvisual'
+import CategoryEyecatch from '../components/category/category-eyecatch'
+// style
+import Category from '../styles/category.module.scss'
 
 export default ({ data }) => {
   return (
     <Layouts>
-      {data.photo.edges.map(({ node }) => (
-        <div>
-          <p> {node.category.category}</p>
-          <Img fluid={node.img.fluid} />
+      <div className={Category.category}>
+        {data.category.edges.map(({ node }) => (
+          <Keyvisual category={node.category} img={node.img.fluid} />
+        ))}
+        <div className="container">
+          <div className={Category.category__list}>
+            {data.photo.edges.map(({ node }, index) => (
+              <div>
+                <p>{index}</p>
+                <CategoryEyecatch title={node.title} img={node.img.fluid} />
+                <p> {node.description.description}</p>
+                {/* <Img fluid={node.img.fluid} /> */}
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
+      </div>
     </Layouts>
   )
 }
 
 export const query = graphql`
   query($category: String!) {
+    category: allContentfulPhotoCategory(
+      filter: { categorySlug: { eq: $category } }
+    ) {
+      edges {
+        node {
+          id
+          category
+          img {
+            fluid {
+              aspectRatio
+              base64
+              sizes
+              src
+              srcSet
+              srcSetWebp
+              srcWebp
+            }
+          }
+        }
+      }
+    }
     photo: allContentfulPhotoPost(
       sort: { fields: contentful_id, order: DESC }
       filter: { category: { elemMatch: { categorySlug: { eq: $category } } } }
@@ -26,6 +63,9 @@ export const query = graphql`
         node {
           id
           title
+          description {
+            description
+          }
           category {
             category
           }
