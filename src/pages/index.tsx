a47fe1fbad8d9e57e44308e82e76e1af4472c578
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-01-22 16:35:01 +0900

    top page修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ import Img from 'gatsby-image'
 
 import Layouts from '../components/layouts'
 import Head from '../components/head'
+import Eyecatch from '../components/top/eyecatch'
+
+import Top from '../styles/top.module.scss'
 
 export default ({ data }) => {
   return (
@@ -16,9 +19,54 @@ export default ({ data }) => {
         pageImgHeight={null}
         pageImgWidth={null}
       />
-      <div className="top">
-        <h1 className="top__title">C-photo</h1>
+      <div className={Top.top}>
+        <h1 className={Top.top__title}>C-photo</h1>
+        <p className={Top.back}>
+          <Img
+            fluid={data.flowers.childImageSharp.fluid}
+            alt=""
+            style={{ height: '100%' }}
+          />
+        </p>
+      </div>
+
+      <div className={Top.categories}>
+        {data.eyecatch.edges.map(({ node }) => (
+          <Eyecatch eyecatchImg={node.img.fluid} category={node.category} />
+        ))}
       </div>
     </Layouts>
   )
 }
+
+export const query = graphql`
+  query {
+    flowers: file(relativePath: { eq: "flowers.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1980) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    eyecatch: allContentfulPhotoCategory(
+      sort: { fields: category, order: DESC }
+    ) {
+      edges {
+        node {
+          category
+          img {
+            fluid(maxWidth: 1980) {
+              base64
+              aspectRatio
+              sizes
+              src
+              srcSet
+              srcSetWebp
+              srcWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`
