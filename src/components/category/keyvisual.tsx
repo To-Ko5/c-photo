d1afdbb74477397f9ee2431eb3d09a323c20df9d
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-01-27 21:12:38 +0900

    一文字目を大文字に変更

--- src/components/category/keyvisual.tsx
+++ src/components/category/keyvisual.tsx
@@ -9,9 +9,16 @@ interface categoryKeyvisual {
 }
 
 export default (props: categoryKeyvisual) => {
+  const firstUpperCase = (title: string) => {
+    const upperCaseTitle = title.charAt(0).toUpperCase() + title.slice(1)
+    console.log()
+    return upperCaseTitle
+  }
   return (
     <div className={Category.keyvisual}>
-      <h1 className={Category.keyvisual__title}>{props.category}</h1>
+      <h1 className={Category.keyvisual__title}>
+        {firstUpperCase(props.category)}
+      </h1>
       <p className={Category.keyvisual__img}>
         <Img fluid={props.img} alt={props.category} />
       </p>
