12c7d9efbca9b098cf54909a6c19bff3ce55be6a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-02-01 21:15:30 +0900

    背景のblurを解除する機能を追加

--- src/components/category/category-keyvisual.tsx
+++ src/components/category/category-keyvisual.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Img, { FluidObject } from 'gatsby-image'
 // style
 import Category from '../../styles/category.module.scss'
@@ -11,12 +11,25 @@ interface categoryKeyvisual {
 }
 
 export default (props: categoryKeyvisual) => {
+  const no_filter = {
+    filter: 'none'
+  }
+
+  const [blur, blurToggle] = useState(false)
+
+  const changeBlur = () => {
+    blurToggle(!blur)
+  }
   return (
     <div className={Category.keyvisual}>
       <h1 className={Category.keyvisual__title}>
         {firstUpperCase(props.category)}
       </h1>
-      <p className={Category.keyvisual__img}>
+      <p
+        className={Category.keyvisual__img}
+        style={blur ? no_filter : undefined}
+        onClick={() => changeBlur()}
+      >
         <Img fluid={props.img} alt={props.category} />
       </p>
     </div>
