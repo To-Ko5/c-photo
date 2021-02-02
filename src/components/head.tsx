c6a7f3de3f2f987634de3879c7854ac62986838f
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-02-02 10:40:32 +0900

    Google font 修正

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -62,7 +62,7 @@ export default (props: metaData) => {
       <meta property="og:image:height" content={String(imgHeight)} />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,400;0,600;0,700;1,400;1,500&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,400;0,500;0,600;1,300;1,400;1,600&display=swap"
         rel="stylesheet"
       />
       <link rel="canonical" href={url} />
