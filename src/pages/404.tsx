84a21cb9fd3ec48f2738e7126a247a119aa79de3
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-02-04 20:40:51 +0900

    styleを修正

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'gatsby'
 // components
 import Layouts from '../components/layouts'
 import Head from '../components/head'
+// style
+import Error from '../styles/error.module.scss'
 
 export default ({ location }) => {
   return (
@@ -10,12 +13,16 @@ export default ({ location }) => {
         pageTitle="ページが見つかりません"
         pageDescription={null}
         pagePath={location.pathname}
-        pageImg={null}
-        pageImgHeight={null}
-        pageImgWidth={null}
       />
-      <div className="404">
-        <h1 className="about__title">お探しのページが見つかりませんでした。</h1>
+      <div className={Error.error}>
+        <div className={Error.error__inner}>
+          <h1 className={Error.error__title}>
+            お探しのページが見つかりませんでした。
+          </h1>
+          <div className={Error.error__back}>
+            <Link to={'/'}>Top Pageに戻る</Link>
+          </div>
+        </div>
       </div>
     </Layouts>
   )
